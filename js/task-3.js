04952217bccde98c4725b4209602f91ba92a0a1e
Utku Sert <utkusert.arc@gmail.com>
2025-07-14 11:23:42 +0300

    2

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,33 @@
+class StringBuilder {
+  #value;
 
-const sortByDescendingFriendCount = (users) => {
-  return users.toSorted((a, b) => b.friends.length - a.friends.length);
-};
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
 
-const usersTask3 = [
-  { name: "Moore Hensley", friends: ["Sharron Pace"], gender: "male" },
-  { name: "Sharlene Bush", friends: ["Briana Decker", "Sharron Pace"], gender: "female" },
-  { name: "Ross Vazquez", friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"], gender: "male" },
-  { name: "Elma Head", friends: ["Goldie Gentry", "Aisha Tran"], gender: "female" },
-  { name: "Carey Barr", friends: ["Jordan Sampson", "Eddie Strong"], gender: "male" },
-  { name: "Blackburn Dotson", friends: ["Jacklyn Lucas", "Linda Chapman"], gender: "male" },
-  { name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"], gender: "female" },
-];
+  getValue() {
+    return this.#value;
+  }
 
-console.log(sortByDescendingFriendCount(usersTask3));
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
